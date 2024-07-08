4f1455cbc020e612d3fed9dd1a8b1160feba3277
TanmayMachkar <tanmaymacho@gmail.com>
2024-07-08 21:20:51 +0530

    working on toast

--- certificate/src/components/Verify/Verify.js
+++ certificate/src/components/Verify/Verify.js
@@ -2,6 +2,7 @@ import { useContext, useState } from 'react';
 import Web3Context from '../../context/Web3Context';
 import Button from '../Button/Button';
 import CryptoJS from 'crypto-js';
+import { toast } from "react-hot-toast";
 import '../IPFS/Input.css';
 
 const Verify = () => {
@@ -9,6 +10,7 @@ const Verify = () => {
   const [file, setFile] = useState(null);
   const [verified, setVerified] = useState(false);
   const [hashh, setHashh] = useState('');
+  const [notVerified, setNotVerified] = useState(1);
 
   const stringToSHA256 = async () => {
     try {
@@ -16,6 +18,8 @@ const Verify = () => {
         console.error('No file selected');
         return;
       }
+
+      toast.loading("Verifying...");
 
       const reader = new FileReader();
       reader.readAsDataURL(file);
@@ -26,15 +30,32 @@ const Verify = () => {
         const hashImage = CryptoJS.SHA256(fileData).toString(CryptoJS.enc.Hex);
         const hexHashImage = '0x' + hashImage;
 
-        const setHash = await certificateContract.getImageHash(hexHashImage);
-        setHashh(setHash);
-        setVerified(true);
+        try {
+          const setHash = await certificateContract.getImageHash(hexHashImage);
+          toast.dismiss();
+          toast.success('Certificate verified');
+          setHashh(setHash);
+          setVerified(true);
+        } catch (error) {
+          if (error.reason === "Hash not found") {
+            toast.dismiss();
+            toast.error('Certificate not valid');
+            setNotVerified(2);
+          } else {
+            throw error;
+          }
+        }
       };
 
       reader.onerror = (error) => {
+        toast.dismiss();
+        toast.error('Certificate not valid');
         console.error('Error converting file:', error);
       };
     } catch (error) {
+      toast.dismiss();
+      toast.error('Certificate not valid');
+      setNotVerified(2);
       console.error(error);
     }
   };
@@ -44,19 +65,16 @@ const Verify = () => {
       <form>
         <input type="file" onChange={(e) => setFile(e.target.files[0])} />
         <Button type="button" onClick={stringToSHA256} label="🔎 Verify" />
-        {verified ? 
-        	<div>
-        		<p>Certificate verified</p>
-        		<img src = {`https://gateway.pinata.cloud/ipfs/${hashh}`} />
-        	</div> 
-        		: 
-        	<p>Certificate not valid</p>
+        {verified &&
+          <div>
+            <p>Certificate verified</p>
+            <img src = {`https://gateway.pinata.cloud/ipfs/${hashh}`} />
+          </div>
         }
+        { notVerified === 2 ? <p>Certificate not valid</p> : ''}
       </form>
     </div>
   );
 };
 
 export default Verify;
-
-//0x9cd89e362ed99f1c95af68c5c4aa01b45deab2fa4c47d60b0c71d817a6b6d4c3
