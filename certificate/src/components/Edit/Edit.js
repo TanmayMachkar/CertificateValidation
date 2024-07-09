b117cba3bc132aba0b203859b6cbe9310a347c5d
TanmayMachkar <tanmaymacho@gmail.com>
2024-07-09 22:07:42 +0530

    toast added for edit will work on watermark and restrictions tomorrow

--- certificate/src/components/Edit/Edit.js
+++ certificate/src/components/Edit/Edit.js
@@ -1,11 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import Web3Context from '../../context/Web3Context';
 import { PinturaEditor } from '@pqina/react-pintura';
 import { getEditorDefaults } from '@pqina/pintura';
 import '@pqina/pintura/pintura.css';
+import toast from 'react-hot-toast';
 
 const Edit = () => {
-  const [inlineResult, setInlineResult] = useState();
-  const [imageSrc, setImageSrc] = useState();
+  const [ inlineResult, setInlineResult ] = useState();
+  const [ imageSrc, setImageSrc ] = useState();
+  const { certificateContract, account } = useContext(Web3Context);
+  const [ accessSpecify, setAccessSpecify ] = useState(false);
+
+  useEffect(() => {
+    const checkRegistered = async() => {
+      try{
+        toast.loading('Getting account info...');
+        const getInfo = await certificateContract.getInfo(account);
+        toast.dismiss();
+        toast.success('Account is registered');
+        setAccessSpecify(true);
+      } catch(error) {
+        setAccessSpecify(false);
+        toast.dismiss();
+        toast.error('Access denied');
+        console.error(error);
+      }
+    }
+    checkRegistered();
+  }, [account]);
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
