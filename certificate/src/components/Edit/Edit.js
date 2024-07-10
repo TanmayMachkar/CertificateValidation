f2498ee4a2f796fd97222e64b96191b5da196c92
TanmayMachkar <tanmaymacho@gmail.com>
2024-07-10 21:51:47 +0530

    restriction on edit done

--- certificate/src/components/Edit/Edit.js
+++ certificate/src/components/Edit/Edit.js
@@ -4,6 +4,7 @@ import { PinturaEditor } from '@pqina/react-pintura';
 import { getEditorDefaults } from '@pqina/pintura';
 import '@pqina/pintura/pintura.css';
 import toast from 'react-hot-toast';
+import './Edit.css';
 
 const Edit = () => {
   const [ inlineResult, setInlineResult ] = useState();
@@ -42,17 +43,37 @@ const Edit = () => {
 
   return (
     <div style={{ height: '80vh' }}>
-      <input type="file" accept="image/*" onChange={handleFileChange} />
+      {
+        !accessSpecify && 
+          <div>
+            <svg
+              width="400"
+              height="400"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M18 10.5C19.6569 10.5 21 11.8431 21 13.5V19.5C21 21.1569 19.6569 22.5 18 22.5H6C4.34315 22.5 3 21.1569 3 19.5V13.5C3 11.8431 4.34315 10.5 6 10.5V7.5C6 4.18629 8.68629 1.5 12 1.5C15.3137 1.5 18 4.18629 18 7.5V10.5ZM12 3.5C14.2091 3.5 16 5.29086 16 7.5V10.5H8V7.5C8 5.29086 9.79086 3.5 12 3.5ZM18 12.5H6C5.44772 12.5 5 12.9477 5 13.5V19.5C5 20.0523 5.44772 20.5 6 20.5H18C18.5523 20.5 19 20.0523 19 19.5V13.5C19 12.9477 18.5523 12.5 18 12.5Z"
+                fill="currentColor"
+              />
+            </svg>
+          </div>
+      }
+      <div className = {accessSpecify ? '' : 'blur'}>
+        {accessSpecify && <input type="file" accept="image/*" onChange={handleFileChange} />}
+        {imageSrc && (
+          <PinturaEditor
+            {...getEditorDefaults()}
+            src={imageSrc}
+            onProcess={(res) => setInlineResult(URL.createObjectURL(res.dest))}
+          />
+        )}
 
-      {imageSrc && (
-        <PinturaEditor
-          {...getEditorDefaults()}
-          src={imageSrc}
-          onProcess={(res) => setInlineResult(URL.createObjectURL(res.dest))}
-        />
-      )}
-
-      {inlineResult && <img src={inlineResult} alt="" />}
+        {inlineResult && <img src={inlineResult} alt="" />}
+      </div>
     </div>
   );
 }
